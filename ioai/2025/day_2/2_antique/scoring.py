e1c0f851b0e89d46c8b7de40da33673594f3abb4
Ícaro silva da rosa linck fróes <icarofroes@gmail.com>
2025-08-21 18:26:25 -0300

    Updated antique solution, baseline and scoring

--- ioai/2025/day_2/2_antique/scoring.py
+++ ioai/2025/day_2/2_antique/scoring.py
@@ -1,2 +1,10 @@
-def score(ground_truth, pred):
-    raise(NotImplementedError)
+import numpy as np
+import pandas as pd
+
+def score(ground_truth_path, pred_path):
+    ground_truth = np.array(pd.read_csv(ground_truth_path))
+    pred = np.array(pd.read_csv(pred_path))
+
+    accuracy = np.mean(ground_truth == pred)
+    
+    return accuracy
